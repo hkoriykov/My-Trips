9e8ace89d33c26dd4094719b3f35dbb4d32bcecf
KORIYKOV <h.koriykov@devnyacement.bg>
2022-04-01 12:15:29 +0300

    Remove unnecessary css styles

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,23 +4,16 @@
 
 export const environment = {
   firebase: {
-    projectId: 'my-trips-9dff9',
-    appId: '1:650684006484:web:6bcd353bb9a351e2d82dc1',
-    storageBucket: 'my-trips-9dff9.appspot.com',
     apiKey: 'AIzaSyBfrILcVAtbF9Y1bHXT_G0bFi15zkHlJq8',
     authDomain: 'my-trips-9dff9.firebaseapp.com',
+    databaseURL:
+      'https://my-trips-9dff9-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'my-trips-9dff9',
+    storageBucket: 'my-trips-9dff9.appspot.com',
     messagingSenderId: '650684006484',
+    appId: '1:650684006484:web:6bcd353bb9a351e2d82dc1',
   },
   production: false,
-};
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyBfrILcVAtbF9Y1bHXT_G0bFi15zkHlJq8',
-  authDomain: 'my-trips-9dff9.firebaseapp.com',
-  projectId: 'my-trips-9dff9',
-  storageBucket: 'my-trips-9dff9.appspot.com',
-  messagingSenderId: '650684006484',
-  appId: '1:650684006484:web:6bcd353bb9a351e2d82dc1',
 };
 
 /*
